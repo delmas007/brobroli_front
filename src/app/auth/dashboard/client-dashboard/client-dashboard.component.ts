ace868ae102083d447a60b3f027590a593996b32
Delon00 <djplebob32@gmail.com>
2024-10-07 18:45:34 +0000

    modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.css 	modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.html 	modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts 	modified:   src/app/auth/dashboard/prestataire-dashboard/prestataire-dashboard.component.ts

--- src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts
+++ src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule,  } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-client-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './client-dashboard.component.html',
   styleUrl: './client-dashboard.component.css'
 })
--- src/app/auth/dashboard/prestataire-dashboard/prestataire-dashboard.component.ts
+++ src/app/auth/dashboard/prestataire-dashboard/prestataire-dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { CommonModule,  } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-prestataire-dashboard',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './prestataire-dashboard.component.html',
   styleUrl: './prestataire-dashboard.component.css'
 })
