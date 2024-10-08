df703c27028a817cef772394dac490bbff20ff03
Delon00 <djplebob32@gmail.com>
2024-10-08 21:57:33 +0000

    new file:   public/media/images/profile.png 	modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.css 	modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.html 	modified:   src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts 	modified:   src/app/auth/register/register.component.html 	modified:   src/styles.css

--- src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts
+++ src/app/auth/dashboard/client-dashboard/client-dashboard.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { CommonModule,  } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
@@ -7,8 +7,26 @@ import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './client-dashboard.component.html',
-  styleUrl: './client-dashboard.component.css'
+  styleUrls: ['./client-dashboard.component.css']
 })
-export class ClientDashboardComponent {
+export class ClientDashboardComponent implements OnInit {
 
+  cards = [
+    {
+      title: 'Bonjour, Alias',
+      text: 'Consultez toutes les collaborations en attente sur votre profil pour débuter un service.',
+      link: 'http://'
+    },
+    { title: 'Carte 2', text: 'Ceci est la carte 2', link: 'https://www.example.com' },
+    { title: 'Carte 3', text: 'Ceci est la carte 3', link: 'https://www.example.com' },
+  ];
+
+  currentCard = 0;
+  constructor() { }
+  ngOnInit(): void {
+    this.startCardRotation();
+  }
+  startCardRotation(): void {
+    setInterval(() => {this.currentCard = (this.currentCard + 1) % this.cards.length;}, 3000);
+  }
 }
