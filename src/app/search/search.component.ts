c3a5a3b9aa0dd7d4f5a79fc1d6860ae9288d20ef
Delon00 <djplebob32@gmail.com>
2024-10-10 15:41:09 +0000

    modified:   src/app/search/search.component.css 	modified:   src/app/search/search.component.html 	modified:   src/app/search/search.component.ts 	modified:   src/styles.css

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
+import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, MatIconModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
